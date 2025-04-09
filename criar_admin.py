92c6d365e4865d712129d0c2ebb1641f035df172
Vlt0rr <vitoralves20112011@gmail.com>
2025-04-08 22:42:06 -0300

    Delete criar_admin.py

--- criar_admin.py
+++
@@ -1,8 +0,0 @@
-from banco_dados import Session, Usuario
-import streamlit_authenticator as stauth
-
-senha_criptografada = stauth.Hasher(["12345"]).generate()[0]
-usuario = Usuario(nome="Vitor", senha=senha_criptografada, telefone="11963918705", admin=False)
-
-Session.add(usuario)
-Session.commit()
