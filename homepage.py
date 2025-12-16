4a456515654526012d88e2391b9e12f37394621f
Vitor Alves <vitoralves20112011@gmail.com>
2025-12-16 00:51:41 -0300

    Atualizar o homepage.py

--- homepage.py
+++ homepage.py
@@ -24,7 +24,7 @@ existing_data = existing_data.dropna(how="all")
 
 nome_usuario = st.selectbox(
     'Nome',
-    ['', 'Adriano', 'Arlunes', 'B. Jesus', 'Bruno', 'Denilson', 'Felipe', 'Genilson', 'Junior', 'José Carlos', 'Neusvaldo', 'Pacheco', 'Ronaldo', 'Roberto', 'Tiago', 'Netanis', 'Wesley', 'Douglas', 'William']
+    ['', 'Adriano', 'Bruno', 'Denilson', 'Felipe', 'Genilson', 'Junior', 'José Carlos', 'Neusvaldo', 'Pacheco', 'Ronaldo', 'Roberto', 'Tiago', 'Netanis', 'Thiago H', 'Wesley', 'Douglas', 'William']
 )
 
 lista_projetos = ['', 'CDS', 'VDS', 'ODS', 'TDS', 'BDS', 'TELM', 'TOT', 'Fabrica', 'Logistica', 'Externo', 'Manutenção', 'Visita a cliente']
