b18025732bbafe33f3fb8d6f5a8ebd41ed94611b
Vitor Alves <vitoralves20112011@gmail.com>
2026-05-11 23:03:27 -0300

    mudei homepage.py
    mudei homepage.py incluindo funcionalidade de acrescentar nome dos funcionarios via google sheets

--- homepage.py
+++ homepage.py
@@ -2,7 +2,7 @@ import streamlit as st
 import datetime
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
-
+from nomes_funcionarios import nome_usuario
 
 secao_usuario = st.session_state
 
@@ -22,10 +22,10 @@ existing_data = conn.read(worksheet="Dados", usecols=list(range(7)), ttl=5)
 existing_data = existing_data.dropna(how="all")
 
 
-nome_usuario = st.selectbox(
-    'Nome',
-    ['', 'Adriano', 'Bruno', 'Denilson', 'Felipe', 'Genilson', 'Junior', 'José Carlos', 'Neusvaldo', 'Pacheco', 'Ronaldo', 'Roberto', 'Tiago', 'Netanis', 'Thiago H', 'Wesley', 'Douglas', 'William']
-)
+#nome_usuario = st.selectbox(
+#    'Nome',
+#    ['', 'Adriano', 'Bruno', 'Denilson', 'Felipe', 'Genilson', 'Junior', 'José Carlos', 'Neusvaldo', 'Pacheco', 'Ronaldo', 'Roberto', 'Tiago', 'Netanis', 'Thiago H', 'Wesley', 'Douglas', 'William']
+#)
 
 lista_projetos = ['', 'CDS', 'VDS', 'ODS', 'TDS', 'BDS', 'TELM', 'TOT', 'Fabrica', 'Logistica', 'Externo', 'Manutenção', 'Visita a cliente']
 
