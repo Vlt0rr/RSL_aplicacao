8eea3eecf5cbd86e8f77987943bc74171ca0d8aa
Vitor Alves <vitoralves20112011@gmail.com>
2026-05-11 23:19:42 -0300

    Encapsulate employee loading in carregar_funcionarios function
    Refactor code to encapsulate functionality in a function.

--- nomes_funcionarios.py
+++ nomes_funcionarios.py
@@ -1,15 +1,17 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-url = "https://docs.google.com/spreadsheets/d/1utRX7L53jEKEMUut4etgksRNf4Xadojf0kw4otIp4tw/edit?usp=sharing"
-
-conn = st.connection("gsheets", type=GSheetsConnection)
-
-df_nomes = conn.read(spreadsheet=url, worksheet="Funcionarios")
-
-lista_nomes = sorted(df_nomes["Nome"].dropna().tolist())
-
-nome_usuario = st.selectbox(
-    "Nome",
-    [""] + lista_nomes
-)
+def carregar_funcionarios():    
+    url = "https://docs.google.com/spreadsheets/d/1utRX7L53jEKEMUut4etgksRNf4Xadojf0kw4otIp4tw/edit?usp=sharing"
+    
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    
+    df_nomes = conn.read(spreadsheet=url, worksheet="Funcionarios")
+    
+    lista_nomes = sorted(df_nomes["Nome"].dropna().tolist())
+    
+    nome_usuario = st.selectbox(
+        "Nome",
+        [""] + lista_nomes
+    )
+    return nome_usuario
