9bd128e27b7042ee60abfc9bfc18b803547792ab
Vitor Alves <vitoralves20112011@gmail.com>
2026-05-11 23:01:38 -0300

    Add files via upload

---
+++ nomes_funcionarios.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+url = "https://docs.google.com/spreadsheets/d/1utRX7L53jEKEMUut4etgksRNf4Xadojf0kw4otIp4tw/edit?usp=sharing"
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df_nomes = conn.read(spreadsheet=url, worksheet="2086210358")
+
+lista_nomes = sorted(df_nomes["Nome"].dropna().tolist())
+
+nome_usuario = st.selectbox(
+    "Nome",
+    [""] + lista_nomes
+)
